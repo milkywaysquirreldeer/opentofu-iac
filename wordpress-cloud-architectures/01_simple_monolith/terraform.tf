acd77a4c29a525440813d2cd9a8bfbaa7d82e186
Kurtis Berger <118536145+milkywaysquirreldeer@users.noreply.github.com>
2026-06-27 14:40:38 -0400

    Move backend config to dedicated file

--- wordpress-cloud-architectures/01_simple_monolith/terraform.tf
+++ wordpress-cloud-architectures/01_simple_monolith/terraform.tf
@@ -7,11 +7,4 @@ terraform {
   }
 
   required_version = "~> 1.12"
-
-  backend "s3" {
-    bucket       = "elastic-wordpress-tf-state-856521070868-us-west-2-an"
-    key          = "backend/terraform.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
-  }
 }
