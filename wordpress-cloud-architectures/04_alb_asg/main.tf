726a62c4d27a2bc3d76e58f6a5b336b177ecfd9b
Kurtis Berger <118536145+milkywaysquirreldeer@users.noreply.github.com>
2026-09-23 20:49:40 -0400

    refactor: move provider blocks into dedicated .tf files

--- wordpress-cloud-architectures/04_alb_asg/main.tf
+++ wordpress-cloud-architectures/04_alb_asg/main.tf
@@ -1,14 +1,3 @@
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      Terraform = "true"
-      Project   = "WordPress"
-    }
-  }
-}
-
 data "aws_availability_zones" "available" {}
 
 locals {
